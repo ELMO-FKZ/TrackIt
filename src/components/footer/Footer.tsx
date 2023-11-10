1ff76814da0e39d4d2f4c92a6c32b6e41e328fdd
EL MOKHTAR <122396078+ELMO-FKZ@users.noreply.github.com>
2023-11-10 08:49:55 +0100

    Adding portfolio url.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
     return (
         <footer className="footer">
             <p> &copy; Copyright {currentYear} </p>
-            Designed & built by <a className="footer__copyright-owner" href="https://www.google.com/" target="_blank" rel="noopener noreferrer">EL MOKHTAR FKHARZ</a>
+            Designed & built by <a className="footer__copyright-owner" href="https://elmo.onrender.com" target="_blank" rel="noopener noreferrer">EL MOKHTAR FKHARZ</a>
         </footer>
     )
 }
